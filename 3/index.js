296bbd0ced52c2e2118df22e19be3c6f57208d25
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-05-21 07:36:24 +0100

    📦 NEW: update the status of class

--- 3/index.js
+++ 3/index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const path = require('path');
 const express = require('../express');
 const idGenerator = require('./util/helpers');
 
@@ -12,7 +13,13 @@ const connection = mysql.createConnection({
   database: 'sample'
 });
 
-connection.connect();
+connection.connect(err => {
+  if (!err) {
+    console.log('Database connected');
+  } else {
+    console.log(err.sqlMessage);
+  }
+});
 
 // configure
 // app.set
@@ -23,7 +30,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-// const users = [];
+
+// allow express go serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // name, age, job, phone
 
@@ -32,11 +41,74 @@ app.get('/', (req, res) => {
   res.send('Welcome to our user app 👯‍♂️');
 });
 
+// Edit a user
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    res.send('Error. You must pass an id');
+  }
+
+  const { name, age, job, phone } = req.body;
+
+  const updateValue = [];
+
+  if (name) {
+    updateValue.push(name);
+  } else {
+    console.log('No parameter: Name: Check the values');
+  }
+  if (age) {
+    updateValue.push(age);
+  } else {
+    console.log('No parameter: Age Check the values');
+  }
+  if (phone) {
+    updateValue.push(phone);
+  } else {
+    console.log('No parameter: phone Check the values');
+  }
+  if (job) {
+    updateValue.push(job);
+  } else {
+    console.log('No parameter: JOB Check the values');
+  }
+
+  if (id) {
+    updateValue.push(id);
+  }
+  console.log(updateValue);
+  /* eslint-disable */
+  const query = `UPDATE users SET ${name ? 'name=?' : ''} ${name && age? ',' : ''} ${ age? 'age=?': '' } ${ age && phone? ',' : ''} ${ phone? 'phone=?': '' } ${ phone && job? ',' : ''} ${job ? 'job=?' : ''} where id =? `;
+  /* eslint-enable */
+  console.log(query.trim());
+  connection.query(query, updateValue, (err, results, fields) => {
+    if (err) {
+      console.log(err.message, err.sqlMessage, err.sqlState);
+      res.send(err.message);
+    }
+    connection.query('SELECT * from USERS where id = ?', [id], function(
+      err,
+      result2,
+      fieldInfo
+    ) {
+      if (err) {
+        console.log(err.message);
+        throw err;
+      }
+
+      console.log(result2.length);
+      // console.log({ fieldInfo });
+      if (!result2) return res.send('Error, user with this ID is alive');
+      res.status(200).json(result2[0]);
+    });
+  });
+});
+
 // create a user
 app.post('/users', (req, res) => {
   // check if required information about a user are sent along in the request.body
 
-  console.log(req.body);
   const { name, age, job, phone } = req.body;
 
   // const name = req.body.name
@@ -87,7 +159,7 @@ app.get('/users/:id', (req, res) => {
   const { id } = req.params;
 
   if (!id) return res.send('Error');
-  connection.query('SELECT * from USERS where id = ?', ['1aq9a3'], function(
+  connection.query('SELECT * from USERS where id = ?', [id], function(
     err,
     results,
     fieldInfo
